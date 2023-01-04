4e8582c870244475f770c479f7c63723f02bd7a5
inytar <pietpiet@fastmail.net>
2023-01-04 21:16:10 +0100

    Add extra length to 3, 6, 9 & 12

--- twelveLinesClockFase.js
+++ twelveLinesClockFase.js
@@ -9,12 +9,14 @@
     for (let i = 0; i < 12; i++) {
       let Phi = i * twoPi/12;
 
-      let x1 = CenterX + outerRadius * sin(Phi) + 2;
-      let x2 = CenterX + innerRadius * sin(Phi) + 2;
-      let y1 = CenterY - outerRadius * cos(Phi) + 2;
-      let y2 = CenterY - innerRadius * cos(Phi) + 2;
+      let extraLength = i % 3 === 0 ? 7 : 0;
 
-      g.drawLine(x1,y1,x2,y2)
+      let x1 = CenterX + outerRadius * sin(Phi) + 2;
+      let x2 = CenterX + (innerRadius - extraLength) * sin(Phi) + 2;
+      let y1 = CenterY - outerRadius * cos(Phi) + 2;
+      let y2 = CenterY - (innerRadius - extraLength) * cos(Phi) + 2;
+
+      g.drawLine(x1,y1,x2,y2);
     }
   };
 })();
